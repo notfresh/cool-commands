f5999501ea82277841bf9b036b29cbe26266308d
notfreshmbp <1195056983@qq.com>
2021-05-09 09:03:19 +0800

    zzz

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #! /bin/bash 
-if [ -f ~/projects/zcommands/alias.rc ]; 
+
+if [-e ~/projects]; then
+    mkdir ~/projects && cd ~/projects
+    git clone https://github.com/notfresh/zcomands  # download the zcomands
+fi
+
+if [ $?==0 -a -f ~/projects/zcommands/alias.rc ]; 
 then
     echo -e "#This import a lot of shortcuts and variables into the local shell \n"\
 "source ~/projects/zcommands/alias.rc\n"\
 "source ~/projects/zcommands/path.rc" >> ~/.zshrc ;
     echo -e "inject ~/projects/zcommands/alias.rc+alias.rc into shell"
+    echo -e "please run `source ~/.zshrc` to refresh the shell "
 else 
     echo "err: zcomands not exists"; 
 fi
 
-source ~/.zshrc
+
